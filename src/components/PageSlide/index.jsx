62517f1047ec052d5d38b3875da0c6a4b5b95a0a
Gabriel Duarte <gduarte93@aol.com>
2019-08-24 10:44:13 -0400

    add parallax to PageSlide

--- src/components/PageSlide/index.jsx
+++ src/components/PageSlide/index.jsx
@@ -1,50 +1,64 @@
-var React = require('react'),
-    data  = require('./mockData.json');
+var React            = require('react'),
+    Component        = React.Component,
+    data             = require('./mockData.json'),
+    Parallax         = require('parallax-js'),
+    pInstance;
 
 require('./PageSlide.css');
 
-function PageSlide() {
-    var title               = data && data.title,
-        background          = data && data.background,
-        overlayColor        = data && data.overlayColor,
-        info                = data && data.info,
-        infoColor           = info && info.color,
-        primary             = infoColor && infoColor.primary,
-        secondary           = infoColor && infoColor.secondary,
-        infoTitle           = info && info.title,
-        infoSub             = info && info.subtitle,
-        desc                = info && info.description,
-        backgroundStyle     = {
-            backgroundImage: `url(${background})`
-        },
-        overlayStyle        = {
-            background: overlayColor
-        },
-        primaryColorStyle   = {
-            color: primary
-        },
-        secondaryColorStyle = {
-            color: secondary
-        };
+class PageSlide extends Component {
+    constructor(props) {
+        super(props);
 
+        this.scene = React.createRef();
+    }
 
-    return (
-        <div className="PageSlide">
-            <div className="PageSlide__background" style={backgroundStyle}></div>
-            <div className="PageSlide__overlay">
-                <div className="PageSlide__overlay--background" style={overlayStyle}>
-                    <div className="PageSlide__overlay--contents">
-                        <div className="PageSlide__name" style={primaryColorStyle}>{title}</div>
-                        <div className="PageSlide__info">
-                            <div className="PageSlide__title" style={primaryColorStyle}>{infoTitle}</div>
-                            <div className="PageSlide__subtitle" style={secondaryColorStyle}>{infoSub}</div>
-                            <div className="PageSlide__description" style={primaryColorStyle}>{desc}</div>
+    componentDidMount() {
+        pInstance = new Parallax(this.scene.current);
+    }
+
+    render() {
+        var title               = data && data.title,
+            background          = data && data.background,
+            overlayColor        = data && data.overlayColor,
+            info                = data && data.info,
+            infoColor           = info && info.color,
+            primary             = infoColor && infoColor.primary,
+            secondary           = infoColor && infoColor.secondary,
+            infoTitle           = info && info.title,
+            infoSub             = info && info.subtitle,
+            desc                = info && info.description,
+            backgroundStyle     = {
+                backgroundImage: `url(${background})`
+            },
+            overlayStyle        = {
+                background: overlayColor
+            },
+            primaryColorStyle   = {
+                color: primary
+            },
+            secondaryColorStyle = {
+                color: secondary
+            };
+
+        return (
+            <div className="PageSlide" ref={this.scene}>
+                <div className="PageSlide__background" style={backgroundStyle} data-depth="0.1"></div>
+                <div className="PageSlide__overlay">
+                    <div className="PageSlide__overlay--background" style={overlayStyle}>
+                        <div className="PageSlide__overlay--contents">
+                            <div className="PageSlide__name" style={primaryColorStyle}>{title}</div>
+                            <div className="PageSlide__info">
+                                <div className="PageSlide__title" style={primaryColorStyle}>{infoTitle}</div>
+                                <div className="PageSlide__subtitle" style={secondaryColorStyle}>{infoSub}</div>
+                                <div className="PageSlide__description" style={primaryColorStyle}>{desc}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } 
 }
 
 module.exports = PageSlide;

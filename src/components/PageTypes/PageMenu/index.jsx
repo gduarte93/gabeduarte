2305c065bf2fa3d8d5704350345ad3cca335adf5
Gabriel Duarte <gduarte93@aol.com>
2020-02-08 17:12:44 -0500

    WIP contact card

--- src/components/PageTypes/PageMenu/index.jsx
+++ src/components/PageTypes/PageMenu/index.jsx
@@ -34,15 +34,19 @@ class PageMenu extends Component {
                     </ul>
                 </div>
                 <div className="Contact">
-                    <div className="Contact__picture">
-                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHzI-lGsnlaag/profile-displayphoto-shrink_200_200/0?e=1586390400&v=beta&t=aKPE5jBuBqutKDk5R3nvkPh0OAzEW408BNjGgbu4pow"></img>
+                    <div className="Contact__left">
+                        <div className="Contact__picture">
+                            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHzI-lGsnlaag/profile-displayphoto-shrink_200_200/0?e=1586390400&v=beta&t=aKPE5jBuBqutKDk5R3nvkPh0OAzEW408BNjGgbu4pow"></img>
+                        </div>
                     </div>
-                    <div className="Contact__name">Gabriel Duarte</div>
-                    <div className="Contact__linkedin">
-                        <a href="https://www.linkedin.com/in/gabeduarte" target="_blank"><i class="fab fa-linkedin"></i></a>
-                    </div>
-                    <div className="Contact__github">
-                        <a href="https://github.com/gduarte93" target="_blank"><i class="fab fa-github"></i></a>
+                    <div className="Contact__right">
+                        <div className="Contact__name">Gabriel Duarte</div>
+                        <div className="Contact__linkedin Contact__link">
+                            <a href="https://www.linkedin.com/in/gabeduarte" target="_blank"><i class="fab fa-linkedin"></i><span>linkedin.com/in/gabeduarte</span></a>
+                        </div>
+                        <div className="Contact__github Contact__link">
+                            <a href="https://github.com/gduarte93" target="_blank"><i class="fab fa-github"></i><span>github.com/gduarte93</span></a>
+                        </div>
                     </div>
                 </div>
             </div>

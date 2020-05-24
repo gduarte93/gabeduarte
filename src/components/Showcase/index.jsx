f72505a6648443f8bfa1a05792193a5abea0be68
Gabriel Duarte <gduarte93@aol.com>
2020-05-24 17:35:04 -0400

    update title/desc for first project

--- src/components/Showcase/index.jsx
+++ src/components/Showcase/index.jsx
@@ -8,12 +8,15 @@ function Showcase(props) {
     var data                 = props && props.data,
         imagePosition        = data && data.imgPos,
         images               = data && data.images,
+        title                = data && data.title,
         description          = data && data.description,
         leftImage            = imagePosition === LEFT,
         backgroundImage      = data && data.backgroundImage,
         backgroundColor      = data && data.backgroundColor,
         imageComp            = images.map((src, idx) => <img key={idx} src={src} className="Showcase__image"/>),
         descComp             = <div className="Showcase__description" dangerouslySetInnerHTML={{__html: description}}/>,
+        titleComp            = title ? <div className="Showcase__title">{title}</div> : null,
+        titleAndDescComp     = <React.Fragment>{titleComp}{descComp}</React.Fragment>,
         imageContainerClass  = 'Showcase__image--container',
         backgroundImageStyle = backgroundImage && {
             backgroundImage: `url(${backgroundImage})`
@@ -27,12 +30,12 @@ function Showcase(props) {
         <div className="Showcase" style={backgroundStyle}>
             <div className={`Showcase__left ${leftImage && imageContainerClass}`}>
                 {
-                    leftImage ? imageComp : descComp
+                    leftImage ? imageComp : titleAndDescComp
                 }
             </div>
             <div className={`Showcase__right ${!leftImage && imageContainerClass}`}>
                 {
-                    leftImage ? descComp : imageComp
+                    leftImage ? titleAndDescComp : imageComp
                 }
             </div>
         </div>

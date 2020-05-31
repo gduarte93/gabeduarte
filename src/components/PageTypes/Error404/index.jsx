be98a8fc493e359cba5f345793ea1418ccd1fe84
Gabriel Duarte <gduarte93@aol.com>
2020-05-31 16:47:28 -0400

    prettify 404 page

--- src/components/PageTypes/Error404/index.jsx
+++ src/components/PageTypes/Error404/index.jsx
@@ -1,8 +1,15 @@
 var React = require('react');
 
+require('./Error404.css');
+
 function Error404() {
     return(
-        <div>Error 404: page not found</div>
+        <div className="Error404">
+            <div className="Error404--text">404 Page Not Found</div>
+            <div className="Error404--subText">
+                <a href="/">Go back home</a>
+            </div>
+        </div>
     );
 }
 module.exports = Error404;
